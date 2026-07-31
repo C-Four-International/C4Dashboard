5b304c53ae88d93e24ccfdd5536dfea55e593645
Yachtman25 <sarcasmman111@gmail.com>
2026-07-30 23:16:33 -0700

    CSP Hardening against XSS Attacks

---
+++ public/ga-init.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+// Default consent mode: denied until accepted
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'analytics_storage': 'denied',
+  'wait_for_update': 500
+});
+gtag('js', new Date());
+
+gtag('config', 'G-WNGWLK05V3');
---
+++ public/theme-init-advanced.js
@@ -0,0 +1,1 @@
+(function(){try{var t=localStorage.getItem('worldmonitor-theme');if(t==='light')document.documentElement.dataset.theme='light';}catch(e){}document.documentElement.classList.add('no-transition');})()
