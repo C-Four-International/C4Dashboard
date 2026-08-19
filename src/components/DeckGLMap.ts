b8df19b6441bf009b38170d9afff7ccdc792c7a2
Yachtman25 <sarcasmman111@gmail.com>
2026-08-19 10:46:02 -0700

    Update DeckGLMap.ts

--- src/components/DeckGLMap.ts
+++ src/components/DeckGLMap.ts
@@ -565,7 +565,7 @@ export class DeckGLMap {
           });
 
           speedUpBtn.addEventListener('click', () => {
-            speedMultiplier = Math.min(speedMultiplier * 2, 86400);
+            speedMultiplier = Math.min(speedMultiplier * 2, 21600); // max 6 hours per second
             if (isPlaying && this.mapTilerPrecipitationLayer && typeof this.mapTilerPrecipitationLayer.animateByFactor === 'function') {
               this.mapTilerPrecipitationLayer.animateByFactor(speedMultiplier);
             }
